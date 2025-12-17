eeb95d4736c41620500d54d4b0f5da32a13b2fb7
Phuctran11 <trancaobaophuc2019@gmai.com>
2025-12-17 08:16:19 +0700

    first tidy

---
+++ test-db-connection.js
@@ -0,0 +1,26 @@
+// run: npm install pg yaml && node .\scripts\test-db-connection.js
+import fs from 'fs';
+import YAML from 'yaml';
+import pkg from 'pg';
+const { Client } = pkg;
+
+const cfg = YAML.parse(fs.readFileSync(new URL('./config.yml', import.meta.url), 'utf8'));
+const db = cfg.db;
+
+const client = new Client({
+  host: db.host,
+  port: db.port,
+  user: db.username,
+  password: db.password,
+  database: db.database,
+});
+try {
+  await client.connect();
+  console.log('Connected to DB OK');
+  const res = await client.query('SELECT now()');
+  console.log(res.rows);
+  await client.end();
+} catch (err) {
+  console.error('DB connection error:', err.message || err);
+  process.exit(1);
+}
